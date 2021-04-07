c0c73f2f41a32e5338562deae0f502c9e1a4b2a9
fyr91 <feng0049@gmail.com>
2021-04-07 17:49:16 +0800

    Updates

---
+++ precache-manifest.a90d6741cf0992006c7ec0265a24fc41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3976da0bae31bcec64b725e95a4745a",
+    "url": "/yirui-s-palace/index.html"
+  },
+  {
+    "revision": "6471b5f42269bf673e9f",
+    "url": "/yirui-s-palace/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "a9deb3d13688c93f327e",
+    "url": "/yirui-s-palace/static/js/2.9f774fc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/yirui-s-palace/static/js/2.9f774fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6471b5f42269bf673e9f",
+    "url": "/yirui-s-palace/static/js/main.211bc019.chunk.js"
+  },
+  {
+    "revision": "fd9d78f07952159579d6",
+    "url": "/yirui-s-palace/static/js/runtime-main.49382574.js"
+  },
+  {
+    "revision": "2a781977897859108c49fc21856409e2",
+    "url": "/yirui-s-palace/static/media/bg.2a781977.jpg"
+  },
+  {
+    "revision": "f5d225706f72cb26e4c13eaabf153187",
+    "url": "/yirui-s-palace/static/media/coming_soon.f5d22570.svg"
+  },
+  {
+    "revision": "9292208373a2552f3f2230f62b1fe2e1",
+    "url": "/yirui-s-palace/static/media/logo.92922083.svg"
+  }
+]);
